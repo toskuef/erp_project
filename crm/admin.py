84f9b4f5750f493f428f09a02fce526eee4b54f3
toskuef <96075802+toskuef@users.noreply.github.com>
2022-10-27 15:18:41 +0300

    Добавил возможность импорта заказов и продуктов

--- crm/admin.py
+++ crm/admin.py
@@ -7,32 +7,57 @@ from .models import *
 @admin.register(Customer)
 class CustomerAdmin(ImportExportModelAdmin):
     pass
+
+
 admin.site.register(SourceCustomer)
+
+
 @admin.register(Address)
 class AddressAdmin(ImportExportModelAdmin):
     pass
+
+
 @admin.register(AddressCountry)
 class AddressCountryAdmin(ImportExportModelAdmin):
     pass
+
+
 @admin.register(AddressArea)
 class AddressAreaAdmin(ImportExportModelAdmin):
     pass
+
+
 @admin.register(AddressRegion)
 class AddressRegionAdmin(ImportExportModelAdmin):
     pass
+
+
 @admin.register(AddressTown)
 class AddressTownAdmin(ImportExportModelAdmin):
     pass
+
+
 @admin.register(AddressStreet)
 class AddressStreetAdmin(ImportExportModelAdmin):
     pass
-admin.site.register(Order)
+
+
+@admin.register(Order)
+class OrderAdmin(ImportExportModelAdmin):
+    pass
+
+
 admin.site.register(TypePay)
 admin.site.register(StatusOrder)
 admin.site.register(Comment)
-admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(ImportExportModelAdmin):
+    pass
+
+
 admin.site.register(Task)
 admin.site.register(Measuring)
 admin.site.register(TypeTask)
 admin.site.register(SocialWebCustomers)
-
