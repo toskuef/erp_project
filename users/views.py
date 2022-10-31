b3157adf2dde21bcdb51bb14e4b47a8e33ca1cfc
toskuef <96075802+toskuef@users.noreply.github.com>
2022-10-31 20:37:02 +0300

    Решена проблема с некорректным URL личного кабинета

--- users/views.py
+++ users/views.py
@@ -1,9 +1,21 @@
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.generic import DetailView
 
 from .models import Profile
 
 
-class ProfileUser(DetailView):
+class LoginRequired(View):
+    """
+    Redirects to login if user is anonymous
+    """
+    @method_decorator(login_required)
+    def dispatch(self, *args, **kwargs):
+        return super(LoginRequired, self).dispatch(*args, **kwargs)
+
+
+class ProfileUser(LoginRequired, DetailView):
     template_name = 'users/profile.html'
     model = Profile
     context_object_name = 'profile'
